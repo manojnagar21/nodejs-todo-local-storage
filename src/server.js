fe1e3f35c0f825301f6eb3e10a34f25f4ab8930d
Mritunjay <ab@c.com>
2026-03-09 15:51:41 +0530

    express server setup done

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (_req, res) => {
+    console.log("Api running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${process.env.DEV_HOST}${PORT}`)
+});
